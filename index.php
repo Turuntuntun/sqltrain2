ecfc05455aa5c4d82a9649e368e4e9cb8139a0cb
Turuntuntun <43621680+Turuntuntun@users.noreply.github.com>
2019-01-16 22:21:49 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,247 @@
+<?php
+	error_reporting(E_ALL);
+    ini_set('display_errors', 'on');
+
+	$host = 'localhost';
+	$user = 'root';
+	$password = 'nfnmzyf40404';
+	$dbName = 'my';
+
+	$link = mysqli_connect($host,$user,$password,$dbName);
+	mysqli_query($link,"SET NAMES 'utf-8'");
+
+	$query = "SELECT *, 3 as res FROM users";
+	$result = mysqli_query($link,$query) or die (mysql_error($link));
+        for( $date = []; $row = mysqli_fetch_assoc($result); $date[] = $row);
+        foreach ($date as $key => $value) {
+            var_dump($value);
+            echo '<br>';
+        }
+    echo "<br>";
+    $query = "SELECT  'eee' as res FROM users";
+	$result = mysqli_query($link,$query) or die (mysql_error($link));
+        for( $date = []; $row = mysqli_fetch_assoc($result); $date[] = $row);
+        foreach ($date as $key => $value) {
+            var_dump($value);
+            echo '<br>';
+        }
+    echo "<br>";
+    $query = "SELECT  '3' as '3' FROM users";
+	$result = mysqli_query($link,$query) or die (mysql_error($link));
+        for( $date = []; $row = mysqli_fetch_assoc($result); $date[] = $row);
+        foreach ($date as $key => $value) {
+            var_dump($value);
+            echo '<br>';
+        }
+    echo "<br>";
+    $query = "SELECT  (val + salary) as 'res' FROM users";
+	$result = mysqli_query($link,$query) or die (mysql_error($link));
+        for( $date = []; $row = mysqli_fetch_assoc($result); $date[] = $row);
+        foreach ($date as $key => $value) {
+            var_dump($value);
+            echo '<br>';
+        }
+    echo "<br>";
+    $query = "SELECT  ( salary - val ) as 'res' FROM users";
+	$result = mysqli_query($link,$query) or die (mysql_error($link));
+        for( $date = []; $row = mysqli_fetch_assoc($result); $date[] = $row);
+        foreach ($date as $key => $value) {
+            var_dump($value);
+            echo '<br>';
+        }
+    echo "<br>";
+    $query = "SELECT  ( salary * val ) as 'res' FROM users";
+	$result = mysqli_query($link,$query) or die (mysql_error($link));
+        for( $date = []; $row = mysqli_fetch_assoc($result); $date[] = $row);
+        foreach ($date as $key => $value) {
+            var_dump($value);
+            echo '<br>';
+        }
+    echo "<br>";
+    $query = "SELECT  (( salary * val )/2) as 'res' FROM users";
+	$result = mysqli_query($link,$query) or die (mysql_error($link));
+        for( $date = []; $row = mysqli_fetch_assoc($result); $date[] = $row);
+        foreach ($date as $key => $value) {
+            var_dump($value);
+            echo '<br>';
+        }
+    echo "<br>";
+    $query = "SELECT  * FROM users WHERE DAY(date) + MONTH(date)<10";
+	$result = mysqli_query($link,$query) or die (mysql_error($link));
+        for( $date = []; $row = mysqli_fetch_assoc($result); $date[] = $row);
+        foreach ($date as $key => $value) {
+            var_dump($value);
+            echo '<br>';
+        }
+    echo "<br>";
+    $query = "SELECT  *, LEFT(login,3) as lef FROM users";
+	$result = mysqli_query($link,$query) or die (mysql_error($link));
+        for( $date = []; $row = mysqli_fetch_assoc($result); $date[] = $row);
+        foreach ($date as $key => $value) {
+            var_dump($value);
+            echo '<br>';
+        }
+    echo "<br>";
+    $query = "SELECT  *, RIGHT(login,3) as lef FROM users";
+	$result = mysqli_query($link,$query) or die (mysql_error($link));
+        for( $date = []; $row = mysqli_fetch_assoc($result); $date[] = $row);
+        foreach ($date as $key => $value) {
+            var_dump($value);
+            echo '<br>';
+        }
+    echo "<br>";
+    $query = "SELECT  *, SUBSTRING(login,3,3) as lef FROM users";
+	$result = mysqli_query($link,$query) or die (mysql_error($link));
+        for( $date = []; $row = mysqli_fetch_assoc($result); $date[] = $row);
+        foreach ($date as $key => $value) {
+            var_dump($value);
+            echo '<br>';
+        }
+    echo "<br>";
+    $query = "SELECT  id,name FROM category UNION SELECT  id,name FROM sub_category";
+	$result = mysqli_query($link,$query) or die (mysql_error($link));
+        for( $date = []; $row = mysqli_fetch_assoc($result); $date[] = $row);
+        foreach ($date as $key => $value) {
+            var_dump($value);
+            echo '<br>';
+        }
+    echo "<br>";
+    $query = "SELECT  CONCAT(val,login) as res FROM users";
+	$result = mysqli_query($link,$query) or die (mysql_error($link));
+        for( $date = []; $row = mysqli_fetch_assoc($result); $date[] = $row);
+        foreach ($date as $key => $value) {
+            var_dump($value);
+            echo '<br>';
+        }
+    echo "<br>";
+    $query = "SELECT  CONCAT(val,login,'!!!') as res FROM users";
+	$result = mysqli_query($link,$query) or die (mysql_error($link));
+        for( $date = []; $row = mysqli_fetch_assoc($result); $date[] = $row);
+        foreach ($date as $key => $value) {
+            var_dump($value);
+            echo '<br>';
+        }
+    echo "<br>";
+    $query = "SELECT  CONCAT_WS('-',val,login) as res FROM users";
+	$result = mysqli_query($link,$query) or die (mysql_error($link));
+        for( $date = []; $row = mysqli_fetch_assoc($result); $date[] = $row);
+        foreach ($date as $key => $value) {
+            var_dump($value);
+            echo '<br>';
+        }
+    echo "<br>";
+    $query = "SELECT  CONCAT(LEFT(val,2),'...') as res FROM users";
+	$result = mysqli_query($link,$query) or die (mysql_error($link));
+        for( $date = []; $row = mysqli_fetch_assoc($result); $date[] = $row);
+        foreach ($date as $key => $value) {
+            var_dump($value);
+            echo '<br>';
+        }
+    echo "<br>";
+    $query = "SELECT val,MIN(salary) as res FROM users  GROUP BY val";
+	$result = mysqli_query($link,$query) or die (mysql_error($link));
+        for( $date = []; $row = mysqli_fetch_assoc($result); $date[] = $row);
+        foreach ($date as $key => $value) {
+            var_dump($value);
+            echo '<br>';
+        }
+    echo "<br>";
+    $query = "SELECT salary,MAX(val) as res FROM users  GROUP BY salary";
+	$result = mysqli_query($link,$query) or die (mysql_error($link));
+        for( $date = []; $row = mysqli_fetch_assoc($result); $date[] = $row);
+        foreach ($date as $key => $value) {
+            var_dump($value);
+            echo '<br>';
+        }
+    echo "<br>";
+    $query = "SELECT val, GROUP_CONCAT(id SEPARATOR '-') as name FROM users GROUP BY val";
+	$result = mysqli_query($link,$query) or die (mysql_error($link));
+        for( $date = []; $row = mysqli_fetch_assoc($result); $date[] = $row);
+        foreach ($date as $key => $value) {
+            var_dump($value);
+            echo '<br>';
+        }
+    echo "<br>";
+    $query = "SELECT * FROM users WHERE salary > (SELECT AVG(salary) FROM users)";
+	$result = mysqli_query($link,$query) or die (mysql_error($link));
+        for( $date = []; $row = mysqli_fetch_assoc($result); $date[] = $row);
+        foreach ($date as $key => $value) {
+            var_dump($value);
+            echo '<br>';
+        }
+    echo "<br>";
+    $query = "SELECT id,val FROM users WHERE val < ((SELECT AVG(val)/ 2 * 3 FROM users) )";
+	$result = mysqli_query($link,$query) or die (mysql_error($link));
+        for( $date = []; $row = mysqli_fetch_assoc($result); $date[] = $row);
+        foreach ($date as $key => $value) {
+            var_dump($value);
+            echo '<br>';
+        }
+    echo "<br>";
+    $query = "SELECT id,salary FROM users WHERE salary = ((SELECT MIN(salary) FROM users))";
+	$result = mysqli_query($link,$query) or die (mysql_error($link));
+        for( $date = []; $row = mysqli_fetch_assoc($result); $date[] = $row);
+        foreach ($date as $key => $value) {
+            var_dump($value);
+            echo '<br>';
+        }
+    echo "<br>";
+    $query = "SELECT id,salary FROM users WHERE salary = ((SELECT MAX(salary) FROM users))";
+	$result = mysqli_query($link,$query) or die (mysql_error($link));
+        for( $date = []; $row = mysqli_fetch_assoc($result); $date[] = $row);
+        foreach ($date as $key => $value) {
+            var_dump($value);
+            echo '<br>';
+        }
+    echo "<br>";
+    $query = "SELECT MAX(salary) as max FROM users WHERE val = 25 ";
+	$result = mysqli_query($link,$query) or die (mysql_error($link));
+        for( $date = []; $row = mysqli_fetch_assoc($result); $date[] = $row);
+        foreach ($date as $key => $value) {
+            var_dump($value);
+            echo '<br>';
+        }
+    echo "<br>";
+    $query = "SELECT ((MAX(val) - MIN(val)) / 2) as avg FROM users";
+	$result = mysqli_query($link,$query) or die (mysql_error($link));
+        for( $date = []; $row = mysqli_fetch_assoc($result); $date[] = $row);
+        foreach ($date as $key => $value) {
+            var_dump($value);
+            echo '<br>';
+        }
+    echo "<br>";
+    $query = "SELECT ((MAX(salary) - MIN(salary)) / 2) as avg FROM users WHERE val = 25";
+	$result = mysqli_query($link,$query) or die (mysql_error($link));
+        for( $date = []; $row = mysqli_fetch_assoc($result); $date[] = $row);
+        foreach ($date as $key => $value) {
+            var_dump($value);
+            echo '<br>';
+        }
+    echo "<br>";
+    $query = "SELECT ((MAX(salary) - MIN(salary)) / 2) as avg FROM users WHERE val = 25";
+	$result = mysqli_query($link,$query) or die (mysql_error($link));
+        for( $date = []; $row = mysqli_fetch_assoc($result); $date[] = $row);
+        foreach ($date as $key => $value) {
+            var_dump($value);
+            echo '<br>';
+        }
+    echo "<br>";
+    $query = "SELECT * FROM page LEFT JOIN category ON  page.category_id = category.id";
+	$result = mysqli_query($link,$query) or die (mysql_error($link));
+        for( $date = []; $row = mysqli_fetch_assoc($result); $date[] = $row);
+        foreach ($date as $key => $value) {
+            var_dump($value);
+            echo '<br>';
+        }
+    echo "<br>";
+    $query = "SELECT * FROM page 
+			    LEFT JOIN category ON  page.category_id = category.id 
+			    LEFT JOIN sub_category ON page.category_id  = sub_category.id";
+	$result = mysqli_query($link,$query) or die (mysql_error($link));
+        for( $date = []; $row = mysqli_fetch_assoc($result); $date[] = $row);
+        foreach ($date as $key => $value) {
+            var_dump($value);
+            echo '<br>';
+        }
+    echo "<br>";
+
